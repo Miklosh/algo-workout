4c31b439aac40101a87ee817846b308ab78ba5e5
unknown <myko0715@WSUA11-0030.netcracker.com>
2015-08-07 19:06:39 +0300

    Binary search in progress

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,36 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+        fillArrayWithTestvalues(arr);
+        performSearch(arr, arr.length - 1, 3);
+    }
+
+    public static boolean performSearch(int[] arr, int thresholdPosition, int searchItem) {
+
+        if (arr.length == 0) return false;
+        if (searchItem > arr.length) return false;
+
+        // check for odd length of array
+//      if arr.length == 2 then use sequential search?
+        if (searchItem == arr[thresholdPosition]) {
+            return true;
+        } else if (searchItem > arr[thresholdPosition]) {
+            thresholdPosition = thresholdPosition / 2;
+            return performSearch(arr, thresholdPosition, searchItem);
+        } else {
+            thresholdPosition = thresholdPosition / 2;
+            return performSearch(arr, thresholdPosition, searchItem);
+        }
+
+    }
+
+    private static void fillArrayWithTestvalues(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+    }
+
+
+}
--- src/main/java/MagicSquare.java
+++ src/main/java/MagicSquare.java
@@ -8,23 +8,6 @@ public class MagicSquare {
     }
 
     private static int[][] process(int[][] matrix, Position position, int acc, int n) {
-
-//        [][] matrix = new int[n];
-        // IF last right border AND there is at least one empty cell above in the same column
-        // THEN go to last left border AND 1 row up AND i.b.o.
-            // IF there is an empty place in 1 s.u. and 1 s.r. cell
-            // THEN  step there AND i.b.o.
-            // IF there is no empty cells 1up 1r THEN move to lower border 1 column right from current AND i.b.o.
-                // (STEP1)IF there is an empty cell 1up 1r THEN step and ibo (repeat)
-                    // IF there is no empty cell 1up 1r BUT IF but its not the last column
-                    // THEN check is there an empty place to the left from current position AND repeat from step (STEP1)
-                        // IF your in the last right column AND there is a row above current position
-                        // THEN go to last right column and iterate one row up ibo
-                            // IF if there is no cells up and left
-                            // THEN move to lower border and move one column right ibo repeat STEP1
-
-
-        // [LAST CHECK end work if every previous check returns false
         if (n%2 == 0) return matrix;
         if (position.row == n/2 && position.column == 0) {
             acc++;
